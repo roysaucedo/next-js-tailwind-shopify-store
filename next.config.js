54772adabc5f8543b7d650a4964ef3a30903110c
roysaucedo <roy.a.saucedo@gmail.com>
2022-09-22 08:02:49 -0700

    Test Change

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  env: {
+    SHOPIFY_STORE_DOMAIN: process.env.SHOPIFY_STORE_DOMAIN,
+    SHOPIFY_STROEFRONT_ACCESSTOKEN: process.env.SHOPIFY_STOREFRON_ACCESSTOKEN
+  },
+  image: {
+    domains: ['cdn.shopify.com']
+  }
+
 }
+
